a84c7446048610436706cc4b624cd0951112129d
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-02-01 10:26:57 +0400

    json server added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,8 @@
   <div id="footer">
       <h2>Now you now how much goverment is taking from our work</h2>
   </div>
+
+  <button>Store data</button>
 </template>
 
 <script>
@@ -17,6 +19,16 @@ export default {
   padding: 15px;
   border-top: solid gray;
   border-radius: 5px;
+}
+button{
+  display: inline-block;
+  padding: 10px 0 10px 0;
+  max-width: 170px;
+  text-align: center;
+  background: rgb(117, 211, 195);
+  margin: 15px auto auto auto;
+  border-radius: 5px;
+  font-size: 20px;
 }
 
 </style>
